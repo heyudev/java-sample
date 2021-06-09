e8db67dcf52d5869b3e6a8ef515eaf8bdb44da36
heyudev <heyudev@gmail.com>
2021-06-09 14:27:40 +0800

    update

--- concurrency/src/main/java/com/heyudev/concurrency/MyThread.java
+++ concurrency/src/main/java/com/heyudev/concurrency/MyThread.java
@@ -1,5 +1,6 @@
 package com.heyudev.concurrency;
 
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -8,36 +9,64 @@ import java.util.concurrent.TimeUnit;
  */
 public class MyThread {
     public static void main(String[] args) {
-        ThreadOne threadOne = new ThreadOne();
-        threadOne.start();
+//        ThreadOne threadOne = new ThreadOne();
+//        threadOne.start();
+//        try {
+//            TimeUnit.MILLISECONDS.sleep(1);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        threadOne.interrupt();
+//        Thread.interrupted();
+//        threadOne.isInterrupted();
+//
+//        System.out.println(threadOne.isInterrupted() + "-" + threadOne.isAlive());
+//        System.out.println(Thread.currentThread().isInterrupted() + "-" + Thread.currentThread().isAlive());
+//        for (int i = 0; i < 10; i++) {
+////            try {
+////                TimeUnit.MILLISECONDS.sleep(10);
+////            } catch (InterruptedException e) {
+////                e.printStackTrace();
+////            }
+//            System.out.println("i = " + i);
+//        }
+//
+//        for (int i = 0; i < 10; i++) {
+////            try {
+////                TimeUnit.MILLISECONDS.sleep(10);
+////            } catch (InterruptedException e) {
+////                e.printStackTrace();
+////            }
+//            System.out.println("ii = " + i);
+//        }
+
+//        Thread2 t1 = new Thread2();
+//        t1.start();
+//
+//        try {
+//            t1.join();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        System.out.println("end");
+
+        Semaphore semaphore = new Semaphore(2);
+        System.out.println(semaphore.availablePermits());
+        semaphore.release();
+        System.out.println(semaphore.availablePermits());
         try {
-            TimeUnit.MILLISECONDS.sleep(1);
+            semaphore.acquire();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        threadOne.interrupt();
-        Thread.interrupted();
-        threadOne.isInterrupted();
-
-        System.out.println(threadOne.isInterrupted() + "-" + threadOne.isAlive());
-        System.out.println(Thread.currentThread().isInterrupted() + "-" + Thread.currentThread().isAlive());
-        for (int i = 0; i < 10; i++) {
-//            try {
-//                TimeUnit.MILLISECONDS.sleep(10);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-            System.out.println("i = " + i);
+        System.out.println(semaphore.availablePermits());
+        try {
+            semaphore.acquire();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+        System.out.println(semaphore.availablePermits());
 
-        for (int i = 0; i < 10; i++) {
-//            try {
-//                TimeUnit.MILLISECONDS.sleep(10);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-            System.out.println("ii = " + i);
-        }
     }
 
 
@@ -64,7 +93,23 @@ public class MyThread {
 
             int i = 0;
             while (!this.isInterrupted()) {
-                System.out.println("--------"+i++);
+                System.out.println("--------" + i++);
+            }
+        }
+    }
+
+
+    static class Thread2 extends Thread {
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("thread2 = " + i);
+
+                try {
+                    TimeUnit.MILLISECONDS.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
