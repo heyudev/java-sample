b93a479bdd1b8f83a20247fc8314873735938dc2
heyudev <heyudev@gmail.com>
2021-06-22 13:00:14 +0800

    update

--- concurrency/src/main/java/com/heyudev/concurrency/MyReference.java
+++ concurrency/src/main/java/com/heyudev/concurrency/MyReference.java
@@ -1,6 +1,10 @@
 package com.heyudev.concurrency;
 
 import java.io.IOException;
+import java.lang.ref.PhantomReference;
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.SoftReference;
+import java.lang.ref.WeakReference;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -14,21 +18,41 @@ public class MyReference {
         }
     }
 
-    public static void main(String[] args) throws InterruptedException, IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         //强引用
         //没有使用才会被回收
+        TimeUnit.SECONDS.sleep(60);
         User user = new User();
         user = null;
         System.gc();
 
-        //软引用
 
-        //弱引用
+        TimeUnit.SECONDS.sleep(60);
+        //软引用 如果一个对象只具有软引用，在内存足够时，垃圾回收器不会回收它；如果内存不足，就会回收这个对象的内存
+        //使用场景：缓存   例如：图片缓存框架
+        //10M
+        SoftReference<byte[]> softReference = new SoftReference<>(new byte[1024 * 1024 * 10]);
+        long maxMemory = Runtime.getRuntime().maxMemory();
+        long freeMemory = Runtime.getRuntime().freeMemory();
+        System.out.println("maxMemory = " + maxMemory);
+        System.out.println("freeMemory = " + freeMemory);
+//        userSoftReference.clear();
+        System.gc();
+        byte[] bytes = new byte[(int) (freeMemory * 2)];
 
+        System.out.println(softReference.get());
+
+        TimeUnit.SECONDS.sleep(60);
+        //弱引用  将对象留在内存的能力不是那么强的引用。当垃圾回收器扫描到只具有弱引用的对象，不管当前内存空间是否足够，都会回收内存
+        //使用场景：ThreadLocal ：
+        WeakReference<User> weakReference = new WeakReference<>(new User());
+        System.out.println(weakReference.get());
+        System.gc();
+
+        TimeUnit.SECONDS.sleep(60);
         //虚引用
-
-
-
+        ReferenceQueue<User> queue = new ReferenceQueue<>();
+        PhantomReference<User> phantomReference = new PhantomReference<>(new User(), queue);
 
 
         System.in.read();
--- concurrency/src/main/java/com/heyudev/concurrency/MyThreadLocal.java
+++ concurrency/src/main/java/com/heyudev/concurrency/MyThreadLocal.java
@@ -1,9 +1,55 @@
 package com.heyudev.concurrency;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author supeng
  * @date 2021/06/21
  */
 public class MyThreadLocal {
 
+    private static final ExecutorService pool = new ThreadPoolExecutor(100, 100, 0, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>(), new ThreadFactoryBuilder().build());
+
+    static class User {
+        private String name;
+
+        public User(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+    public static void main(String[] args) {
+        ThreadLocal<String> threadLocal = new ThreadLocal<>();
+        threadLocal.set("1");
+
+        InheritableThreadLocal<String> inheritableThreadLocal = new InheritableThreadLocal<>();
+        inheritableThreadLocal.set("2");
+
+        System.out.println(threadLocal.get() + "--1--" + inheritableThreadLocal.get());
+
+        pool.execute(() -> System.out.println(threadLocal.get() + "--2--" + inheritableThreadLocal.get()));
+
+        new Thread(() -> System.out.println(threadLocal.get() + "--3--" + inheritableThreadLocal.get())).start();
+
+        //value 强引用
+        User user = new User("value");
+        ThreadLocal<User> threadLocal1 = new ThreadLocal<>();
+        threadLocal1.set(user);
+
+        // 回收
+        threadLocal1.remove();
+    }
 }
