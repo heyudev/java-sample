985c308d21d37ca55a6f213de51ce685658620b6
heyudev <heyudev@gmail.com>
2021-07-05 14:03:29 +0800

    update

---
+++ distributed-transaction-business/src/main/java/com/heyudev/distributed/transaction/business/DistributedTransactionBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.heyudev.distributed.transaction.business;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedTransactionBusinessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedTransactionBusinessApplication.class, args);
+    }
+
+}
---
+++ distributed-transaction-business/src/test/java/com/heyudev/distributed/transaction/business/DistributedTransactionBusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heyudev.distributed.transaction.business;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedTransactionBusinessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
